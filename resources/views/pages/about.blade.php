0038b46548c6a8d29d2d057aa415078bd8c42566
Audrius <aubuka@gmail.com>
2016-08-22 23:15:24 +0100

    add about page header

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
-@section('title')
-  About
-@endsection
-
 @section('title_meta')
   @if (!Auth::guest())
       <a href="{{ route('page_edit', 'About') }}">Edit</a>
@@ -11,5 +7,14 @@
 @endsection
 
 @section('content')
-  {!! $about->body !!}
+  <div class="header">
+    <img src="/images/about_img.png" alt="" />
+    <div class="text inner-container">
+      <h2>Design is not just what it looks like,<br/>feels like, design is how it works.</h2>
+      <div class="author"> - Steve Jobs -</div>
+    </div>
+  </div>
+  <article class="inner-container">
+    {!! $about->body !!}
+  </article>
 @endsection
