b17dba8b8ad982cd0d6967b93cb30bd7613ac822
Audrius <aubuka@gmail.com>
2016-08-22 22:48:20 +0100

    fix fatal homepage error when work has no files attached

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,21 @@
         <div class="work-wrapper">
             <div class="masonry">
                 @foreach ($works as $work)
-                    <div class="grid-item">
-                        <!--<h3><a href="{{ url('/'.$work->slug) }}">{{ $work->title }}</a>
-                        @if(!Auth::guest())
-                            @if($work->active == '1')
-                                <button class="btn" style="float: right"><a href="{{ url('work/'.$work->slug.'/edit')}}">Edit Work</a></button>
-                            @else
-                                <button class="btn" style="float: right"><a href="{{ url('work/'.$work->slug.'/edit')}}">Edit Draft</a></button>
-                            @endif
-                        @endif
-                        </h3>
-                        -->
-                            <a href="{{ $work->slug }}"><img src="/storage/{{ $work->files->first()->filename }}" alt="{{ $work->title }}" /></a>
-                    </div>
+                    @if (count($work->files))
+                        <div class="grid-item">
+                            <!--<h3><a href="{{ url('/'.$work->slug) }}">{{ $work->title }}</a>
+                            </h3>
+                            -->
+                                <a href="{{ $work->slug }}"><img src="/storage/{{ $work->files->first()->filename }}" alt="{{ $work->title }}" /></a>
+                                @if(!Auth::guest())
+                                    @if($work->active == '1')
+                                        <button class="btn" style="float: right"><a href="{{ url('work/'.$work->slug.'/edit')}}">Edit Work</a></button>
+                                    @else
+                                        <button class="btn" style="float: right"><a href="{{ url('work/'.$work->slug.'/edit')}}">Edit Draft</a></button>
+                                    @endif
+                                @endif
+                        </div>
+                    @endif
                 @endforeach
             </div>
         </div>
