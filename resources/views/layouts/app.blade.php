6c05a50e504850f5ff12fa68ad2ae2cbdc166b47
Audrius <aubuka@gmail.com>
2016-08-26 14:27:53 +0100

    style messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
         <link rel="stylesheet" href="/css/normalize.css">
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     </head>
@@ -44,18 +45,18 @@
         <div class="page-row page-row-expanded">
           <div class="container">
               @if (Session::has('message'))
-                  <div class="flash alert-info">
-                      <p class="panel-body">{{ Session::get('message') }}</p>
+                  <div class="flash isa_success inner-container">
+                      <i class="fa fa-info-circle"></i>
+                      <span>{{ Session::get('message') }}</span>
                   </div>
               @endif
               @if ($errors->any())
-                  <div class='flash alert-danger'>
-                      <ul class="panel-body">
-                          @foreach ( $errors->all() as $error )
-                              <li>{{ $error }}</li>
-                          @endforeach
-                      </ul>
-                </div>
+                @foreach ( $errors->all() as $error )
+                  <div class='flash isa_error inner-container'>
+                      <i class="fa fa-times-circle"></i>
+                      <span>{{ $error }}</span>
+                  </div>
+                @endforeach
               @endif
               <div class="page-info inner-container">
                   <h1>@yield('title')</h1>
