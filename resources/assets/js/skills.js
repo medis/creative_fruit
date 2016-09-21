20cc26fdb78daa502066ef97c8e763d6e760d029
Audrius <aubuka@gmail.com>
2016-09-21 20:38:21 +0100

    initialise skills on page load, run animation when scrolled down

--- resources/assets/js/skills.js
+++ resources/assets/js/skills.js
@@ -2,7 +2,7 @@
   if (!$('.skills-wrapper .skill-chart').length) {
     return;
   }
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
+
   var Skill = function(selector, colours, percent, text, delay) {
     this.selector = selector;
     this.colours = colours;
@@ -15,7 +15,7 @@
       .sort(null);
 
     this.w = 230;
-    this. h=230;
+    this.h = 230;
 
     this.outerRadius = (this.w/2.3)-10;
     this.innerRadius = this.outerRadius-12;
@@ -140,80 +140,74 @@
           fill: this.colours[0],
           'font-size':'50px'
       });
-
-
-    var arcTweenOld=function(transition, percent, oldValue) {
-      transition.attrTween("d", function (d) {
-        var newAngle= -1 * (this.percent/100)*(2*Math.PI);
-        var interpolate = d3.interpolate(d.endAngle, newAngle);
-        var interpolateCount = d3.interpolate(oldValue, this.percent);
-        return function (t) {
-          d.endAngle = interpolate(t);
-          var pathForegroundCircle = arcLine(d);
-          this.middleTextCount.text(Math.floor(interpolateCount(t))+'%');
-          var pathDummyCircle = arcDummy(d);
-          var coordinate = pathDummyCircle.split("L")[1].split("A")[0];
-          this.endLine.style('opacity', 1).attr('transform', 'translate(' + coordinate + ')' + 'rotate(' + (d.endAngle * 180/Math.PI) + ')');
-          return pathForegroundCircle;
-        };
-      });
-    };
-
   }
 
-  Skill.prototype.animate_process = function() {
-    console.log(this.pathForeground);
+  Skill.prototype.arcTweenOld = function(transition, self, percent, oldValue) {
+    transition.attrTween("d", function (d) {
+      var newAngle= -1 * (percent/100)*(2*Math.PI);
+      var interpolate = d3.interpolate(d.endAngle, newAngle);
+      var interpolateCount = d3.interpolate(oldValue, percent);
+      return function (t) {
+        d.endAngle = interpolate(t);
+        var pathForegroundCircle = self.arcLine(d);
+        self.middleTextCount.text(Math.floor(interpolateCount(t))+'%');
+        var pathDummyCircle = self.arcDummy(d);
+        var coordinate = pathDummyCircle.split("L")[1].split("A")[0];
+        self.endLine.style('opacity', 1).attr('transform', 'translate(' + coordinate + ')' + 'rotate(' + (d.endAngle * 180/Math.PI) + ')');
+        return pathForegroundCircle;
+      };
+    });
+  };
+
+  Skill.prototype.animate_process = function(self) {
     var oldValue = 0;
-    this.pathForeground.transition()
+    self.pathForeground.transition()
       .duration(750)
       .ease('cubic')
-      .call(arcTweenOld, this.percent, oldValue);
+      .call(self.arcTweenOld, self, self.percent, oldValue);
 
-    this.middleTextCount.transition()
+    self.middleTextCount.transition()
       .delay(1500)
       .duration(750)
       .style('opacity', 0);
 
-    this.middleTextTitle.transition()
+    self.middleTextTitle.transition()
       .delay(2250)
       .duration(500)
       .style('opacity', 1);
 
-    oldValue = this.percent;
-    this.percent = (Math.random() * 60) + 20;
+    oldValue = self.percent;
+    self.percent = (Math.random() * 60) + 20;
   }
 
   Skill.prototype.animate = function() {
-    setTimeout(this.animate_process, this.delay);
+    var self = this;
+    setTimeout(function() {
+      self.animate_process(self);
+    }, self.delay);
   }
-
 
   var ended = false;
   var controller = new ScrollMagic.Controller();
   var skills = [];
   var timeout = 700;
   var i = 0;
-  var delay = timeout * i++;
+  var delay = 0;
   $('.skills-wrapper .skill-chart').each(function() {
+    delay = timeout * i++;
     skills.push(new Skill('#' + $(this).attr('id'), JSON.parse($(this).attr('data-colors')), $(this).attr('data-percent'), $(this).attr('data-text'), delay));
   });
-  /*new ScrollMagic.Scene({
+
+  new ScrollMagic.Scene({
     triggerElement: '#trigger-skills'
   })
   .addTo(controller)
   .on("start", function (e) {
     if (!ended) {
-      if ($('.skills-wrapper .skill-chart').length) {
-        var timeout = 700;
-        var i = 0;
-        $('.skills-wrapper .skill-chart').each(function() {
-          var delay = timeout * i++;
-          skills.push(new Skill('#' + $(this).attr('id'), JSON.parse($(this).attr('data-colors')), $(this).attr('data-percent'), $(this).attr('data-text'), delay));
-        });
+      for (var i=0; i < skills.length; i++) {
+        skills[i].animate();
       }
       ended = true;
     }
-	});*/
-  console.log(skills[0]);
-  console.log(skills[0].animate());
+	});
 })(jQuery);
